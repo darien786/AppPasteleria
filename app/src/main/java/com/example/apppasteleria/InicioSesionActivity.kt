00487088f857ba0c37bf646ba197725b3666017e
Darien Baltazar <darienbaltazar2@gmail.com>
2024-01-29 23:47:43 -0600

    Initial commit

---
+++ app/src/main/java/com/example/apppasteleria/InicioSesionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.apppasteleria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioSesionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_sesion)
+    }
+}
